6120c287e56d9228653df9365183c26028bf3c59
kouki0610 <mitsu.daigaku@gmail.com>
2026-03-13 23:05:01 +0900

    Add async function to load and display companies

---
+++ data/script.js
@@ -0,0 +1,34 @@
+async function loadCompanies(){
+
+const res = await fetch("data/companies.json");
+const companies = await res.json();
+
+const list = document.getElementById("companyList");
+const search = document.getElementById("search");
+
+function render(filter=""){
+list.innerHTML="";
+
+companies
+.filter(c=>c.name.includes(filter))
+.forEach(c=>{
+const li=document.createElement("li");
+
+li.textContent=c.name;
+
+li.onclick=()=>{
+location.href="company.html?id="+c.id;
+};
+
+list.appendChild(li);
+});
+
+}
+
+search.oninput=()=>render(search.value);
+
+render();
+
+}
+
+loadCompanies();
